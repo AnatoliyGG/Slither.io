c26ee062fbecab628b5038203e20bed2b1322690
AnatoliyGG <mazkovgg@gmail.com>
2025-03-29 16:21:32 +0300

    add Vector 2 Vector 3, new variable _mouseInput

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -4,8 +4,17 @@ using Unity.Netcode;
 
 public class PlayerController : NetworkBehaviour
 {
+    private Camera _mainCamera;
+    private Vector3 _mouseInput;
+    private void Initialize()
+    {
+        _mainCamera = Camera.main;
+    }
     private void Update()
     {
-        
+        _mouseInput.x = Input.mousePosition.x;
+        _mouseInput.y = Input.mousePosition.y;
+        _mouseInput.y = _mainCamera.nearClipPlane;
+        Vector3 mouseWorldCoordinates = _mainCamera.ScreenToWorldPoint((Vector3)mousePositionScreen);
     }
 }
