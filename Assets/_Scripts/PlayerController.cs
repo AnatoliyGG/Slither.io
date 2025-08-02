2a46444453355d08e41c720ea5748767e03e3008
AnatoliyGG <mazkovgg@gmail.com>
2025-08-02 22:35:32 +0300

    PlayerController.cs, Movement
    I've been dealing with the Update, ScreenToWorldPoint, MoveTowards, and variables functions all day today.

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -2,8 +2,9 @@ using System;
 using UnityEngine;
 using Unity.Netcode;
 
-public class PlayerController : NetworkBehaviour
+public class PlayerController : NetworkBehaviour 
 {
+    [SerializeField] private float speed = 3f;
     private Camera _mainCamera;
     private Vector3 _mouseInput;
     private void Initialize()
@@ -12,9 +13,17 @@ public class PlayerController : NetworkBehaviour
     }
     private void Update()
     {
+        // Movement
         _mouseInput.x = Input.mousePosition.x;
         _mouseInput.y = Input.mousePosition.y;
-        _mouseInput.y = _mainCamera.nearClipPlane;
-        Vector3 mouseWorldCoordinates = _mainCamera.ScreenToWorldPoint((Vector3)mousePositionScreen);
+        _mouseInput.z = _mainCamera.nearClipPlane;
+        Vector3 mouseWorldCoordinates = _mainCamera.ScreenToWorldPoint((Vector3)_mouseInput);
+        transform.position = Vector3.MoveTowards(current:transform.position, target:mouseWorldCoordinates,maxDistanceDelta:Time.deltaTime * speed);
+        
+        // Rotate
+        if (mouseWorldCoordinates != transform.position)
+        {
+            
+        }
     }
 }
